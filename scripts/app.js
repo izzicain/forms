fa25395797c2202ccd5e132f77cd52f3eccf2534
“James <jtraver@gogoair.com>
2017-07-13 20:10:15 -0500

    code up to date

--- scripts/app.js
+++ scripts/app.js
@@ -8,8 +8,27 @@ window.onload = function(event) {
   nameBtn.onclick = function(event) {
     // console.log(event);
     console.log(nameInput.value);
+    createRow('Welcome aboard, ' + nameInput.value);
   }
 
   console.log(nameInput, nameBtn);
 
+}
+
+// this function creates and adds rows to our app
+function createRow(words) {
+  console.log(words);
+  // we need a new div
+  var row = document.createElement('div');
+  var col = document.createElement('div');
+  var p = document.createElement('p');
+  p.innerHTML = words;
+  row.append(col);
+  row.className = 'row';
+  col.className = 'col s12';
+  col.append(p);
+  console.log(row);
+  // append the whole thing to app
+  var app = document.getElementById('app');
+  app.append(row);
 }
